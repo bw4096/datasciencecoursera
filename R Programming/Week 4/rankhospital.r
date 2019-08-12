2c452c9240972bfd996f47e2abe7f4bb5b967c02
Bryan Willauer <bw4096@yahoo.com>
2019-08-12 07:06:12 -0400

    Create rankhospital.r

---
+++ R Programming/Week 4/rankhospital.r
@@ -0,0 +1,26 @@
+rankhospital <- function(state, outcome, num = "best") {
+    ## Read outcome data
+    df1 <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    df <- df1[complete.cases(df1),]
+    
+    ## Check that state and outcome are valid
+    if(num == "best")
+        num <- 1
+    if(outcome == "heart attack")
+        outcome_choice <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+    else if(outcome == "pneumonia")
+        outcome_choice <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+    else if(outcome == "heart failure")
+        outcome_choice <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+    ## Return hospital name in that state with the given rank
+    ## 30-day death rate
+    
+    state_outcome <- select(df, Hospital.Name, State, outcome_choice)
+    state_name <- state
+    answer_list <- filter(state_outcome, State == state_name)
+    answer <- arrange(answer_list, as.numeric(answer_list[,3])) 
+    
+    answer1 <- answer[order(as.numeric(answer[,3]), answer[,1]),]
+    #answer2 <- answer1[complete.cases(answer1),]
+    return(answer1[num, 1])
+}
