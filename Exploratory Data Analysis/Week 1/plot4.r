9dcdfb9ad54679c7e5883a859f7a38ce04c95b8b
Bryan Willauer <bw4096@yahoo.com>
2019-09-07 20:37:45 -0400

    Update plot4.r

--- Exploratory Data Analysis/Week 1/plot4.r
+++ Exploratory Data Analysis/Week 1/plot4.r
@@ -1,7 +1,7 @@
 ## Aim of this script is to 
 ## 1. read the household_power_consumption.txt file
 ## 2. subset for data taken from 2 days: 2007-02-01 and 2007-02-02
-## 3. generate a plot of different submetering vs time
+## 3. generate 4 plots in 1 space (GAP vs. time, Vol vs. time, submetering vs. time and GRP vs. time)
 
 ## Parameters: none
 ## Assumes household_power_consumption.txt file located in working dir
@@ -19,9 +19,13 @@ data$Time <- format(data$Time, format="%H:%M:%S")
 new_data <- subset(data, Date == "2007-02-01" | Date =="2007-02-02")
 
 ## generate plot
-png("plot3.png", width=480, height=480)
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+plot(new_data$FullTimeDate , as.numeric(new_data$Global_active_power),type="l", xlab="Day", ylab="Global Active Power (kilowatts)")
+plot(new_data$FullTimeDate, as.numeric(new_data$Voltage),type = "l",xlab="datetime", ylab="Voltage") 
 plot(new_data$FullTimeDate , as.numeric(new_data$Sub_metering_1) ,type="l", xlab="Day", ylab="Energy sub metering")
 lines(new_data$FullTimeDate, new_data$Sub_metering_2,type="l", col= "red")
 lines(new_data$FullTimeDate, new_data$Sub_metering_3,type="l", col= "blue")
 legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
+plot(new_data$FullTimeDate, as.numeric(new_data$Global_reactive_power), type="l", xlab="datetime", ylab="Global_reactive_power")
 dev.off()
